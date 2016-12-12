8bbeddc6c4168715611a84aec70bb77d7352dd23
wangyang59 <wangyang59@baidu.com>
2016-12-12 13:55:38 -0800

    video seqToseq model starts working

--- demo/video/dataprovider.py
+++ demo/video/dataprovider.py
@@ -35,9 +35,9 @@ def hook(settings, src_path, is_generating, file_list, **kwargs):
     else:
         settings.slots = {
             'source_language_word':
-            integer_value_sequence(len(settings.src_dict)),
+            dense_vector_sequence(len(settings.src_dict)),
             'sent_id':
-            integer_value_sequence(len(open(file_list[0], "r").readlines()))
+            dense_vector_sequence(len(open(file_list[0], "r").readlines()))
         }
     
     settings.logger.info("src dim %d" % (settings.src_dim))
@@ -54,15 +54,12 @@ def process(settings, file_name):
     dataHandler = data_handler.BouncingMNISTDataHandler(num_frames, batch_size, image_size, 
                                             num_digits, step_length, "./data/mnist.h5")
     
-
     for i in xrange(100):
         batch = dataHandler.GetBatch()[0]
         for j in xrange(batch.shape[0]):
             seq = map(list, list(batch[j].reshape(-1, settings.src_dim)))
-            settings.logger.info("generated %d %d" % (i, j))
             yield {
                 'source_image_seq': seq,
                 'target_image_seq': seq[0:-1],
                 'target_image_seq_next': seq[1:]
             }
-            settings.logger.info("generated %d %d" % (i, j))
